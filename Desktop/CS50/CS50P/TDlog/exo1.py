b113084a3d3fd167b0f740a3a1bd414e6fceba36
AyemanBougdira <Ayeman.bougdira@centrale-casablanca.ma>
2023-10-05 09:46:50 +0100

    ma-nouvelle-branche Exo1: Ayeman Bougdira

---
+++ Desktop/CS50/CS50P/TDlog/exo1.py
@@ -0,0 +1,23 @@
+#Exo11
+
+class Item:
+    def __init__(self, price, weight):
+        self.price = price
+        self.weight = weight
+
+    def __str__(self):
+        return f"{self.price} and {self.weight}"
+    
+
+
+def get_item():
+    price = input('Price:')
+    weight = input('Weight')
+    return Item(price, weight)
+
+def main():
+    item = get_item()
+    print(item)
+
+
+main()
